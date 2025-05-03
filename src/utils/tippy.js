242b59ebe3bec5b16edb72b5c4589e9440a7ade9
Ville Joe Catig <vjjoecatig@gmail.com>
2025-05-03 22:33:32 +0800

    feat: update Reach Out button, footer, and refactor tippy.js
    - Added LinkedIn link to the "Reach Out" button
    - Set a fixed color for the tippy tooltip
    - Added tippy hover text "... LinkedIn" on the Reach Out button
    - Added social media icons in the footer using Remixicon
    - Refactored tippy.js (Tooltip) for cleaner code

--- src/utils/tippy.js
+++ src/utils/tippy.js
@@ -1,11 +1,27 @@
 import tippy from "tippy.js";
 
-const tippyJs = () => {
-  tippy(".featured__link", {
-    content: "🚫 Link Unavailable",
+const initializeToolTip = (selector, content) => {
+  tippy(selector, {
+    content: content,
     theme: "translucent",
     animation: "shift-away",
   });
+};
+
+const tippyJs = () => {
+  // REACH OUT LINK
+  document.querySelectorAll("a").forEach((link) => {
+    if(link.textContent.trim().toUpperCase() === "REACH OUT")
+      initializeToolTip(link, "🔗 LinkedIn");
+  });
+
+  // FEATURED PROJECT LINK
+  initializeToolTip(".featured__link", "🚫 Link Unavailable");
+
+  // SOCIAL LINKS
+  initializeToolTip(".facebook", "Facebook");
+  initializeToolTip(".linkedin", "LinkedIn");
+  initializeToolTip(".github", "GitHub");
 };
 
 export default tippyJs; 
